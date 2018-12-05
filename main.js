10d7aa12ce957b9e577c0d90c66fadb1d7a9e705
Rogerio Angeliski <angeliski@hotmail.com>
2018-12-05 19:22:11 -0200

    Adicionando colisão e refatorando

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const matriz = [
-  [4,0,2,0],
+  [4,0,2,2],
   [0,0,0,0], 
   [0,0,0,0], 
   [2,0,0,0]
 ];
+
+let matrizFusao = [[],[], [], []];
 
 function escreverLinha(text) {
   document.write(text);
@@ -45,16 +47,52 @@ function moverParaBaixo() {
 
 }
 
+function proximaPosicao(matriz, linha, coluna, movimento) {
+  if(movimento === 'direita'){
+    return matriz[linha][coluna -1];
+  }
+} 
+
+function zerarProximaPosicao(matriz, linha, coluna, movimento) {
+  if(movimento === 'direita'){
+    matriz[linha][coluna -1] = 0; 
+  }
+} 
+
+function posicaoAtual(matriz, linha, coluna){
+  return matriz[linha][coluna];
+}
+
 function moverParaDireita() {
-  for(let linha = 0; linha < matriz.length; linha++) {
-    for (let coluna = matriz[linha].length - 1; coluna >= 0; coluna--) {
-      if(matriz[linha][coluna] == 0){
-        matriz[linha][coluna] = matriz[linha][coluna - 1] || 0;
-        matriz[linha][coluna - 1] = 0;
-      } 
-    }
+  const movimento = 'direita';
+  let teveMovimento = true;
+  matrizFusao = [[],[], [], []];
+
+  while(teveMovimento) {
+    teveMovimento = false;
+    for(let linha = 0; linha < matriz.length; linha++) {
+      for (let coluna = matriz[linha].length - 1; coluna >= 0; coluna--) {
+        if((posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0) 
+        || (posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) 
+        && proximaPosicao(matriz, linha, coluna, movimento) != 0) || 0) {
+          teveMovimento = true;
+          if((posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) || 0) && !matrizFusao[linha][coluna]) {
+            matriz[linha][coluna] += proximaPosicao(matriz, linha, coluna, movimento) || 0;
+            matrizFusao[linha][coluna] = true;
+          } else if(posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0){
+            matriz[linha][coluna] = proximaPosicao(matriz, linha, coluna, movimento) || 0;
+          }else {
+            teveMovimento = false;
+          }
+          if(teveMovimento){
+            zerarProximaPosicao(matriz, linha, coluna, movimento);
+          }
+        }         
+      }
+    }  
   }
 
+  
   escreverMatrizNaTela();
 }
 
