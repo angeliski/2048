0329bdf72f372783701f4fbac5357f7561445a07
Rogerio Angeliski <angeliski@hotmail.com>
2018-12-04 22:26:44 -0200

    Criando primeira versão de movimentos

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const matriz = [
   [4,0,2,0],
   [0,0,0,0], 
-  [2,0,0,0], 
-  [0,0,0,0]
+  [0,0,0,0], 
+  [2,0,0,0]
 ];
 
 function escreverLinha(text) {
@@ -14,22 +14,68 @@ function pularLinha() {
 };
 
 
-function moverParaCima () {
-
+function moverParaCima ()  {
+  let qtdLinhas = matriz.length -1;
+  for(let linha = 0; linha < qtdLinhas; linha++) {
+    let qtdColunas =  matriz[linha].length;
+    for (let coluna = 0; coluna < qtdColunas; coluna++) {
+     if(matriz[linha][coluna] == 0){
+      matriz[linha][coluna] = matriz[linha + 1][coluna] || 0;
+      if(linha + 1 <= qtdLinhas) {
+        matriz[linha  + 1][coluna] = 0;
+      }
+     }
+    }
+  }
+  escreverMatrizNaTela();
 }
 
+
 function moverParaBaixo() {
+  for(let linha = matriz.length - 1; linha > 0; linha--) {
+    for (let coluna = 0; coluna < matriz[linha].length; coluna++) {
+      if(matriz[linha][coluna] == 0){
+        matriz[linha][coluna] = matriz[linha - 1][coluna] || 0;
+        matriz[linha - 1][coluna] = 0;
+      } 
+    }
+  }
+
+  escreverMatrizNaTela();
+
 }
 
 function moverParaDireita() {
-  
+  for(let linha = 0; linha < matriz.length; linha++) {
+    for (let coluna = matriz[linha].length - 1; coluna >= 0; coluna--) {
+      if(matriz[linha][coluna] == 0){
+        matriz[linha][coluna] = matriz[linha][coluna - 1] || 0;
+        matriz[linha][coluna - 1] = 0;
+      } 
+    }
+  }
+
+  escreverMatrizNaTela();
 }
 
 function moverParaEsquerda () {
-
+  for(let linha = 0; linha < matriz.length; linha++) {
+    let qtdColunas =  matriz[linha].length;
+    for (let coluna = 0; coluna < qtdColunas; coluna++) {
+     if(matriz[linha][coluna] == 0){
+      matriz[linha][coluna] = matriz[linha][coluna + 1] || 0;
+      if(coluna + 1 < qtdColunas) {
+        matriz[linha][coluna + 1] = 0;
+      }
+     }
+    }
+  }
+  escreverMatrizNaTela();
 }
 
 function escreverMatrizNaTela () {
+  escreverLinha('-----------------------------------');
+  pularLinha();
   for(let linha = 0; linha < matriz.length; linha++) {
     for (let coluna = 0; coluna < matriz[linha].length; coluna++) {
       escreverLinha(' | ');
