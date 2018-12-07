e3ca1025172b1672558efea80bdf8f84f6c3300e
Rogerio Angeliski <angeliski@hotmail.com>
2018-12-06 22:12:18 -0200

    Aplicando refactor nas funções

--- main.js
+++ main.js
@@ -21,120 +21,52 @@ function pularLinha() {
     escreverLinha('<br>');
 };
 
-function adicionarNumeroAleatorio() {
-  matrizTentativas = [[],[], [], []];
-  
-  let tentarInserir = true; 
-  while(tentarInserir) {
-    let linha = Math.floor(Math.random() * 4);
-    let coluna = Math.floor(Math.random() * 4); 
 
-    if(posicaoAtual(matriz, linha, coluna) === 0 && posicaoAtual(matrizTentativas, linha, coluna) !== 'X') {
-      matriz[linha][coluna] = 2;
-      tentarInserir = false;
-    }else {
-      matrizTentativas[linha][coluna] = 'X';
-    }
-
-    if(tentarInserir && todaAMatrizEstaPreenchida()){
-      tentarInserir = false;
-    }
-  }
-
+function posicaoAtual(matriz, linha, coluna){
+  return matriz[linha][coluna];
 }
 
-function todaAMatrizEstaPreenchida() {
-
-  for(let linha = 0; linha < matriz.length; linha++) {
-    for (let coluna = 0; coluna < matriz[linha].length; coluna++) {
-      if (posicaoAtual(matriz, linha, coluna) === 0) {
-        return false;
-      }
-    }
-  }
-  return true;
+function posicaoAtualEstaVazia(matriz, linha, coluna) {
+  return posicaoAtual(matriz, linha, coluna) === 0;
 }
 
-
-function moverParaCima ()  {
-  const movimento = 'cima';
-  let teveMovimento = true;
-  matrizFusao = [[],[], [], []];
-  
-  while(teveMovimento ){
-    teveMovimento = false;
-    let qtdLinhas = matriz.length - 1;
-    for(let linha = 0; linha < qtdLinhas; linha++) {
-      let qtdColunas =  matriz[linha].length;
-      for (let coluna = 0; coluna < qtdColunas; coluna++) {
-       if((posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0) 
-        || (posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) 
-        && proximaPosicao(matriz, linha, coluna, movimento) != 0) || 0) {
-          teveMovimento = true;
-          if((posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) || 0) 
-          && !matrizFusao[linha][coluna] 
-          && !proximaPosicao(matrizFusao, linha, coluna, movimento)) {
-            matriz[linha][coluna] += proximaPosicao(matriz, linha, coluna, movimento) || 0;
-            matrizFusao[linha][coluna] = true;
-          } else if(posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0){
-            matriz[linha][coluna] = proximaPosicao(matriz, linha, coluna, movimento) || 0;
-          }else {
-            teveMovimento = false;
-          }          
-
-          if(linha + 1 <= qtdLinhas && teveMovimento) {
-            zerarProximaPosicao(matriz, linha, coluna, movimento);
-          }
-       }
-      }
-    }
-  }
-  
+function proximaPosicaoNaoFoiCombinada(matrizFusao, linha, coluna, movimento) {
+  return !proximaPosicao(matrizFusao, linha, coluna, movimento);
 }
 
+function posicaoAtualNaoFoiCombinada(matrizFusao, linha, coluna) {
+  return !matrizFusao[linha][coluna];
+}
 
-
-function moverParaBaixo() {
-  const movimento = 'baixo';
-  let teveMovimento = true;
-  matrizFusao = [[],[], [], []];
-  
-  while(teveMovimento ) {
-    teveMovimento = false;
-    for(let linha = matriz.length - 1; linha > 0; linha--) {
-      for (let coluna = 0; coluna < matriz[linha].length; coluna++) {
-        if((posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0) 
-        || (posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) 
-        && proximaPosicao(matriz, linha, coluna, movimento) != 0) || 0) {
-          teveMovimento = true;
-          if((posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) || 0) 
-          && !matrizFusao[linha][coluna]
-          && !proximaPosicao(matrizFusao, linha, coluna, movimento)) {
-            matriz[linha][coluna] += proximaPosicao(matriz, linha, coluna, movimento) || 0;
-            matrizFusao[linha][coluna] = true;
-          } else if(posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0){
-            matriz[linha][coluna] = proximaPosicao(matriz, linha, coluna, movimento) || 0;
-          }else {
-            teveMovimento = false;
-          }        
-          if(teveMovimento){
-            zerarProximaPosicao(matriz, linha, coluna, movimento);
-          }
-        } 
-      }
-    }
+function aindaNaoChegouAoFim(linha, coluna, quantidade, movimento) {
+  if(movimento == 'cima') {
+    return linha + 1 <= quantidade;
+  }else if(movimento == 'esquerda') {
+    return coluna + 1 <= quantidade;
   }
+}
+
+function proximaPosicaoEstaVazia(matriz, linha, coluna, movimento) {
+  return proximaPosicao(matriz, linha, coluna, movimento) === 0;
+}
+
+function posicaoAtualEhIgualAProxima(matriz, linha, coluna, movimento) {
+  return posicaoAtual(matriz, linha, coluna) === proximaPosicao(matriz, linha, coluna, movimento);
+}
+
+function proximaPosicaoEstaPreenchida(matriz, linha, coluna, movimento) {
+  return proximaPosicao(matriz, linha, coluna, movimento) !== 0;
 }
 
 function proximaPosicao(matriz, linha, coluna, movimento) {
   if(movimento === 'direita'){
-    return matriz[linha][coluna - 1];
+    return matriz[linha][coluna - 1] || 0;
   } else if(movimento === 'esquerda') {
     return matriz[linha][coluna + 1] || 0;
   } else if(movimento === 'cima') {
     return matriz[linha + 1][coluna] || 0;
   } else if(movimento === 'baixo') {
-    return matriz[linha - 1][coluna];
+    return matriz[linha - 1][coluna] || 0;
   }
 } 
 
@@ -150,8 +82,121 @@ function zerarProximaPosicao(matriz, linha, coluna, movimento) {
   }
 } 
 
-function posicaoAtual(matriz, linha, coluna){
-  return matriz[linha][coluna];
+
+
+
+function adicionarNumeroAleatorio() {
+  matrizTentativas = [[],[], [], []];
+  
+  let tentarInserir = true; 
+  while(tentarInserir) {
+    let linha = Math.floor(Math.random() * 4);
+    let coluna = Math.floor(Math.random() * 4); 
+
+    if(posicaoAtualEstaVazia(matriz,linha, coluna) && posicaoAtual(matrizTentativas, linha, coluna) !== 'X') {
+      matriz[linha][coluna] = 2;
+      tentarInserir = false;
+    }else {
+      matrizTentativas[linha][coluna] = 'X';
+    }
+
+    if(tentarInserir && todaAMatrizEstaPreenchida()){
+      tentarInserir = false;
+    }
+  }
+
+}
+
+function todaAMatrizEstaPreenchida() {
+  for(let linha = 0; linha < matriz.length; linha++) {
+    for (let coluna = 0; coluna < matriz[linha].length; coluna++) {
+      if (posicaoAtualEstaVazia(matriz, linha, coluna)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function moverDoComecoParaOFim(matriz, matrizFusao, linha, coluna, movimento, qtdLinhas) {
+  let teveMovimento = false;
+  if ((posicaoAtualEstaVazia(matriz, linha, coluna) && proximaPosicaoEstaPreenchida(matriz, linha, coluna, movimento))
+    || (posicaoAtualEhIgualAProxima(matriz, linha, coluna, movimento) && proximaPosicaoEstaPreenchida(matriz, linha, coluna, movimento))) {
+    teveMovimento = true;
+    if (posicaoAtualEhIgualAProxima(matriz, linha, coluna, movimento)
+      && posicaoAtualNaoFoiCombinada(matrizFusao, linha, coluna)
+      && proximaPosicaoNaoFoiCombinada(matrizFusao, linha, coluna, movimento)) {
+      matriz[linha][coluna] += proximaPosicao(matriz, linha, coluna, movimento);
+      matrizFusao[linha][coluna] = true;
+    }
+    else if (posicaoAtualEstaVazia(matriz, linha, coluna) && proximaPosicaoEstaPreenchida(matriz, linha, coluna, movimento)) {
+      matriz[linha][coluna] = proximaPosicao(matriz, linha, coluna, movimento);
+    }
+    else {
+      teveMovimento = false;
+    }
+    if (aindaNaoChegouAoFim(linha, coluna, qtdLinhas, movimento) && teveMovimento) {
+      zerarProximaPosicao(matriz, linha, coluna, movimento);
+    }
+  }
+  return teveMovimento;
+}
+
+function moverDoFimParaOComeco(matriz, matrizFusao, linha, coluna, movimento) {
+  let teveMovimento = false;
+  if ((posicaoAtualEstaVazia(matriz, linha, coluna) && proximaPosicaoEstaPreenchida(matriz, linha, coluna, movimento))
+    || (posicaoAtualEhIgualAProxima(matriz, linha, coluna, movimento) && proximaPosicaoEstaPreenchida(matriz, linha, coluna, movimento))) {
+    teveMovimento = true;
+    if (posicaoAtualEhIgualAProxima(matriz, linha, coluna, movimento)
+      && posicaoAtualNaoFoiCombinada(matrizFusao, linha, coluna)
+      && proximaPosicaoNaoFoiCombinada(matrizFusao, linha, coluna, movimento)) {
+      matriz[linha][coluna] += proximaPosicao(matriz, linha, coluna, movimento);
+      matrizFusao[linha][coluna] = true;
+    }
+    else if (posicaoAtualEstaVazia(matriz, linha, coluna) && proximaPosicaoEstaPreenchida(matriz, linha, coluna, movimento)) {
+      matriz[linha][coluna] = proximaPosicao(matriz, linha, coluna, movimento);
+    }
+    else {
+      teveMovimento = false;
+    }
+    if (teveMovimento) {
+      zerarProximaPosicao(matriz, linha, coluna, movimento);
+    }
+  }
+  return teveMovimento;
+}
+
+
+function moverParaCima ()  {
+  const movimento = 'cima';
+  let teveMovimento = true;
+  matrizFusao = [[],[], [], []];
+  
+  while(teveMovimento ){
+    teveMovimento = false;
+    let qtdLinhas = matriz.length - 1;
+    for(let linha = 0; linha < qtdLinhas; linha++) {
+      let qtdColunas =  matriz[linha].length;
+      for (let coluna = 0; coluna < qtdColunas; coluna++) {
+       teveMovimento = teveMovimento || moverDoComecoParaOFim(matriz, matrizFusao, linha, coluna, movimento, qtdLinhas);
+      }
+    }
+  }
+}
+
+function moverParaBaixo() {
+  const movimento = 'baixo';
+  let teveMovimento = true;
+  matrizFusao = [[],[], [], []];
+  
+  while(teveMovimento) {
+    teveMovimento = false;
+    for(let linha = matriz.length - 1; linha > 0; linha--) {
+      for (let coluna = 0; coluna < matriz[linha].length; coluna++) {
+        teveMovimento = teveMovimento || moverDoFimParaOComeco(matriz, matrizFusao, linha, coluna, movimento); 
+      }
+    }
+  }
 }
 
 function moverParaDireita() {
@@ -163,26 +208,7 @@ function moverParaDireita() {
     teveMovimento = false;
     for(let linha = 0; linha < matriz.length; linha++) {
       for (let coluna = matriz[linha].length - 1; coluna >= 0; coluna--) {
-
-        if((posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0) 
-        || (posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) 
-        && proximaPosicao(matriz, linha, coluna, movimento) != 0) || 0) {
-          teveMovimento = true;
-          if((posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) || 0) 
-          && !matrizFusao[linha][coluna]
-          && !proximaPosicao(matrizFusao, linha, coluna, movimento)) {
-            matriz[linha][coluna] += proximaPosicao(matriz, linha, coluna, movimento) || 0;
-            matrizFusao[linha][coluna] = true;
-          } else if(posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0){
-            matriz[linha][coluna] = proximaPosicao(matriz, linha, coluna, movimento) || 0;
-          }else {
-            teveMovimento = false;
-          }
-          if(teveMovimento){
-            zerarProximaPosicao(matriz, linha, coluna, movimento);
-          }
-        }
-
+        teveMovimento = teveMovimento || moverDoFimParaOComeco(matriz, matrizFusao, linha, coluna, movimento);
       }
     }  
   }
@@ -198,31 +224,12 @@ function moverParaEsquerda () {
     for(let linha = 0; linha < matriz.length; linha++) {
       let qtdColunas =  matriz[linha].length;
       for (let coluna = 0; coluna < qtdColunas; coluna++) {
-       if( (posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0) 
-        || (posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) 
-        && proximaPosicao(matriz, linha, coluna, movimento) != 0) || 0){
-          teveMovimento = true;
-
-          if((posicaoAtual(matriz,linha,coluna) == proximaPosicao(matriz, linha, coluna, movimento) || 0) 
-          && !matrizFusao[linha][coluna]
-          && !proximaPosicao(matrizFusao, linha, coluna, movimento)) {
-            matriz[linha][coluna] += proximaPosicao(matriz, linha, coluna, movimento) || 0;
-            matrizFusao[linha][coluna] = true;
-
-          } else if(posicaoAtual(matriz,linha,coluna) == 0 && proximaPosicao(matriz, linha, coluna, movimento) != 0){
-            matriz[linha][coluna] = proximaPosicao(matriz, linha, coluna, movimento) || 0;
-          }else {
-            teveMovimento = false;
-          }
-
-        if(coluna + 1 < qtdColunas && teveMovimento) {
-          zerarProximaPosicao(matriz, linha, coluna, movimento);
-        }
-       }
+        teveMovimento = teveMovimento || moverDoComecoParaOFim(matriz, matrizFusao, linha, coluna, movimento, qtdColunas);
       }
     }
   }
 }
+
 
 function escreverMatrizNaTela () {
   escreverLinha('-----------------------------------');
